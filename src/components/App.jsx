7867898a485c96fa58ca9b70ecd37030fd0ed611
Mike Peterson <mpeterson2004@gmail.com>
2021-06-04 11:15:24 -0600

    pulling in collections on page load and some minor navbar changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import logo from '../logo.svg';
 import './App.css';
 import React, {Component} from 'react';
 import Axios from 'axios';
@@ -9,15 +8,29 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      renderIndex:'tbd'
+      renderIndex:'home',
+      collections:[]
     }
+  }
+
+  async getCollections(){
+    let collections = await Axios.get('http://127.0.0.1:8000/')
+    console.log(collections)
+    this.setState({collections:collections})
+
+  }
+
+  componentDidMount(){
+    this.getCollections();
   }
 
 
   render(){
+    console.log("RENDERING")
+    console.log(this.state.collections)
     return (
       <div className="text-light bg-dark">
-        <Navbar/>
+        <Navbar render={this.state.renderIndex}/>
        HAHAHAHAHAHA
       </div>
     );
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,26 +1,24 @@
 
 function Navbar(props){
-    return(<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="#" tabindex="-1">Collection Management</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Card Management</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>);
+    if(props.render === 'home'){
+        return(
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="Home">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="Collections" tabindex="-1">Collection Management</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="cards" tabindex="-1" aria-disabled="true">Card Management</a>
+                        </li>
+                    </ul>
+                </div>
+        </nav>
+    );
+  }
 }
 
 export default Navbar;
