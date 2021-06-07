bfbe0efeaba435cbba54b7b3706ffd7e803d8395
Mike Peterson <mpeterson2004@gmail.com>
2021-06-07 13:04:07 -0600

    added functionality to show the definition of a flash card

--- src/components/cardViewer/cardViewer.jsx
+++ src/components/cardViewer/cardViewer.jsx
@@ -18,21 +18,25 @@ class CardViewer extends Component{
 
     render(){
         if(this.state.card != this.props.card){
-            this.setState({card:this.props.card})
+            this.setState({
+                card:this.props.card,
+                renderIndex:'card'
+            })
+            return("Updating")
         }
         return(
             <table className="table">
                 <tbody className="table-secondary">
                     <tr>
                         <td ClassName='table table-secondary'>Term:</td>
-                        <td ClassName='table table-secondary'><button className='btn btn-secondary'>{this.state.card.term}</button></td>
+                        <td ClassName='table table-secondary'><button className='btn btn-secondary' onClick={()=>{this.showDef()}}>{this.state.card.term}</button></td>
                     </tr>
                     <tr>
                         <td>
                         Definition:
                         </td>
                         <td>
-                            {this.state.renderIndex === 'card'&&<button className='btn btn-secondary'>Click here for Definition</button>}
+                            {this.state.renderIndex === 'card'&&<button className='btn btn-secondary' onClick={()=>{this.showDef()}}>Click here for Definition</button>}
                             {this.state.renderIndex === 'def'&&<div className='btn btn-secondary'>{this.state.card.definition}</div>}
                         </td>
                     </tr>
